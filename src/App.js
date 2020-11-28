d47d6bfb20fcfea90ec507d6496e7ba7dbaa5f80
Waltteri Rintala <rintala.waltteri@gmail.com>
2020-11-28 18:59:24 +0200

    added animations and more css styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useRef, useState} from 'react'
+import frame from './frame.png'
 
 
-const Canvas = () => {
-  console.log("render")
-  
+const Canvas = () => {  
   const canvasRef = useRef(null)
+
+  // TODO: yhdistä tilat yhdeksi olioksi
   const [depth, setDepth] = useState(12)
   const [ratio, setRatio] = useState(120)
   const [anglemultiplier, setAnglemultiplier] = useState(10)
-  const [initialLength, setInitialLength] = useState(18)
-  const [skewness, setSkewness] = useState(10)
+  const [initialLength, setInitialLength] = useState(16)
+  const [skewness, setSkewness] = useState(8)
+  const [threeBranches, setThreeBranches] = useState(false)
   const [leafStyle, setLeafStyle] = useState("summer")
+  const [autumnLeavesParameters, setAutumnLeavesParameters] = useState({odds: 0.8, range: 400, constant: 0})
+
+  const [controllersVisible, setControllersVisible] = useState(true)
 
 
 
@@ -31,12 +36,20 @@ const Canvas = () => {
     ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)]
     ctx.fill()
   }
+
   
   useEffect(() => {
+
+    if (threeBranches && depth > 8) {
+      setDepth(8)
+      window.alert("For performance reasons we have to limit the depth when branches split to three")
+      return null
+    }
+
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
-    canvas.width = 1000
-    canvas.height = 600
+    canvas.width = 800
+    canvas.height = 500
     const width = canvas.width
     const height = canvas.height
 
@@ -59,15 +72,24 @@ const Canvas = () => {
       const endY = prevEndY - Math.cos(angle) * length
       const endX2 = prevEndX + Math.sin(angle2) * length
       const endY2 = prevEndY - Math.cos(angle2) * length
-  
-  
-      return([
+
+      const branches = [
         {startX: prevEndX, startY: prevEndY, endX, endY, angle},
-        {startX: prevEndX, startY: prevEndY, endX: endX2, endY: endY2, angle: angle2},
-        ])
+        {startX: prevEndX, startY: prevEndY, endX: endX2, endY: endY2, angle: angle2}
+        ]
+
+      if (threeBranches){
+        const middleAngle = prevAngle + s1 / 2 + s2 /2
+        const endXMiddle = prevEndX + Math.sin(middleAngle) * length
+        const endYMiddle = prevEndY - Math.cos(middleAngle) * length
+        const middleBranch = {startX: prevEndX, startY: prevEndY, endX: endXMiddle, endY: endYMiddle, angle: middleAngle}
+        return(branches.concat(middleBranch))
+      }
+  
+      return(branches)
     }
 
-    const leafCords = (prevCoordinates) => {
+    const leafCords = (prevCoordinates, isAutumn) => {
       const prevEndX = prevCoordinates.endX
       const prevEndY = prevCoordinates.endY
       const prevStartX = prevCoordinates.startX
@@ -83,10 +105,26 @@ const Canvas = () => {
       const r1Y = prevEndY - Math.cos(angle + Math.PI / 4) * length / 2 * 1.4
       const r2X = prevEndX + Math.sin(angle - Math.PI / 4) * length / 2 * 1.4
       const r2Y = prevEndY - Math.cos(angle - Math.PI / 4) * length / 2 * 1.4
+
+      const result = {startX: prevEndX, startY: prevEndY, endX, endY, r1X, r1Y, r2X, r2Y}
+
+      if (isAutumn){
+        if (Math.random() > autumnLeavesParameters.odds) {
+          const randX = (Math.floor(Math.random() * autumnLeavesParameters.range +autumnLeavesParameters.constant))
+          const randY = (Math.floor(Math.random() * autumnLeavesParameters.range +autumnLeavesParameters.constant))
+
+          for (let key in result) {
+            if ([result.startY, result.endY, result.r1Y, result.r2Y].includes(result[key])) {
+              result[key] += randY
+            } 
+            else {
+            result[key] = result[key] + randX
+            }
+          }
+        }
+      }
       
-  
-  
-      return({startX: prevEndX, startY: prevEndY, endX, endY, r1X, r1Y, r2X, r2Y})
+      return(result)
     }
 
     const initialLine = {startX: width / 2, startY: height, endX: width / 2, endY: height * (1 - initialLength / 100), angle: 0}
@@ -104,42 +142,92 @@ const Canvas = () => {
       }
     }
 
-    const recurseDrawTree = (cords, count, seed, color) => {
-      const cordsWithoutLength = cords
-      delete cordsWithoutLength.length
+    const recurseDrawTree = (cords, count, seed, color, isAutumn) => {
 
       seed++
-      drawLine(context, cordsWithoutLength)
+      drawLine(context, cords)
       const nextCoordinates = nextBranchCords(cords)
 
       if (seed === count || count <= 0 || !count) {
         if (color !== "none") {
-          const leafCoordinates = leafCords(cords)
+          const leafCoordinates = leafCords(cords, isAutumn)
           drawLeaf(context, leafCoordinates, color)
         }
         return null
       }
-      recurseDrawTree(nextCoordinates[0], count, seed, color)
-      recurseDrawTree(nextCoordinates[1], count, seed, color)
+      recurseDrawTree(nextCoordinates[0], count, seed, color, isAutumn)
+      recurseDrawTree(nextCoordinates[1], count, seed, color, isAutumn)
+      if (threeBranches){
+        recurseDrawTree(nextCoordinates[2], count, seed, color, isAutumn)
+      }
     }
 
     const col = color(leafStyle)
-    recurseDrawTree(initialLine, depth, 0, col)
-  }, [depth, ratio, anglemultiplier, initialLength, leafStyle, skewness])
+    const isAutumn = leafStyle === "autumn" ? true : false
+
+    recurseDrawTree(initialLine, depth, 0, col, isAutumn)
+  }, [depth, ratio, anglemultiplier, initialLength, leafStyle, skewness, threeBranches, autumnLeavesParameters.odds, autumnLeavesParameters.range, autumnLeavesParameters.constant])
   
+  const handleGrowAnimation = () => {
+    for (let i = 1; i <= depth; i++) {
+      setTimeout(() => {setDepth(i)}, i * 1000 / 3)
+    }
+  }
+
+  const handleLeafDrop = () => {
+    const timePerStep = 1 / 4
+    const stepsFace1 = 20
+    setLeafStyle("autumn")
+    for (let i = 1; i <= stepsFace1; i++) {
+      setTimeout(() => {setAutumnLeavesParameters({odds: (stepsFace1 - i) / stepsFace1, range: 100 * i, constant: 0})}, i * 1000 * timePerStep)
+    }
+    const stepsFace2 = 5
+    for (let i = 1; i <= stepsFace2; i++) {
+      setTimeout(() => {setAutumnLeavesParameters({odds: 0, range: 100 * stepsFace1, constant: i * 100})}, (stepsFace1 + i) * 1000 * timePerStep)
+    }
+    setTimeout(() => {
+      setAutumnLeavesParameters({odds: 0.8, range: 400, constant: 0})
+      setLeafStyle(leafStyle)
+    }, (stepsFace1 + stepsFace2 + 1) * 1000 * timePerStep)
+  }
+
   return (
     <>
-      <canvas id="can" ref={canvasRef}/>
+      <div id="canvas-wrap" onClick={() => setControllersVisible(!controllersVisible)}>
+        <canvas id="can" 
+          style={{background: leafStyle === "none" ? "linear-gradient(white, white)" : null}} 
+          ref={canvasRef}
+        />
+        <img id="frame" src={frame} alt="frame" />
+      </div>
+      {controllersVisible ?
       <div id="controllers">
+        <h1>Options:</h1>
         <div>
-          <label htmlFor="depth">Number of branches</label>
+          Animations:
+          <div id="anim-buttons">
+            <button onClick={handleGrowAnimation}>Grow</button>
+            <button onClick={handleLeafDrop}>Drop</button>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="depth">Depth</label>
           <input
             id="depth"
             type="number"
             min="1" 
-            max="14"
+            max={threeBranches ? "8" :"14"}
             value={depth}
             onChange={({ target }) => setDepth(parseInt(target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="third-branch">Split to three</label>
+          <input 
+            type="checkbox" 
+            id="third-branch"
+            checked={threeBranches}
+            onChange={() => setThreeBranches(!threeBranches)}
           />
         </div>
         <div>
@@ -177,7 +265,7 @@ const Canvas = () => {
         </div>
         <div>
           <label htmlFor="skewness">Skewness</label>
-          <input 
+          <input
             type="range" 
             id="skewness"
             min="-30" 
@@ -187,36 +275,42 @@ const Canvas = () => {
           />
         </div>
         <div className="radio-wrap">
-          Leaf style:
-          <div>
-            <input 
-              type="radio" 
-              id="autumn"
-              checked={leafStyle === "autumn" ? true : false}
-              onChange={() => setLeafStyle("autumn")}
-            />
-            <label htmlFor="autumn">Autumn</label>
-          </div>
-          <div>
-            <input 
-              type="radio" 
-              id="summer"
-              checked={leafStyle === "summer" ? true : false}
-              onChange={() => setLeafStyle("summer")}
-            />
-            <label htmlFor="summer">Summer</label>
-          </div>
-          <div>
-            <input 
-              type="radio" 
-              id="none"
-              checked={leafStyle === "none" ? true : false}
-              onChange={() => setLeafStyle("none")}
-            />
-            <label htmlFor="none">None</label>
+          Season:
+          <div className="radio-select">
+            <div>
+              <input 
+                type="radio" 
+                id="autumn"
+                checked={leafStyle === "autumn" ? true : false}
+                onChange={() => setLeafStyle("autumn")}
+              />
+              <label htmlFor="autumn">Autumn</label>
+            </div>
+            <div>
+              <input 
+                type="radio" 
+                id="summer"
+                checked={leafStyle === "summer" ? true : false}
+                onChange={() => setLeafStyle("summer")}
+              />
+              <label htmlFor="summer">Summer</label>
+            </div>
+            <div>
+              <input 
+                type="radio" 
+                id="none"
+                checked={leafStyle === "none" ? true : false}
+                onChange={() => setLeafStyle("none")}
+              />
+              <label htmlFor="none">None</label>
+            </div>
           </div>
         </div>
-      </div>
+        <div id="opt-info">
+          Click the painting to hide the options
+        </div>
+      </div> :
+      null}
     </>
     
   )
